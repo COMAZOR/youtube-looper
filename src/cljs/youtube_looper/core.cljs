24d0bac2d36a7b943bc597c76e17f1ed3c00e253
Wilker Lucio <wilkerlucio@gmail.com>
2015-01-29 22:38:30 -0300

    correctly handle youtube page changes and update version

--- src/cljs/youtube_looper/core.cljs
+++ src/cljs/youtube_looper/core.cljs
@@ -1,7 +1,7 @@
 (ns youtube-looper.core
   (:require-macros [cljs.core.async.macros :refer [go]]
                    [wilkerdev.util.macros :refer [dochan]])
-  (:require [cljs.core.async :refer [chan put! <! close!] :as async]
+  (:require [cljs.core.async :refer [chan put! <! >! close!] :as async]
             [cljs.core.match :refer-macros [match]]
             [wilkerdev.util :refer [format]]
             [wilkerdev.util.dom :as dom]
@@ -60,13 +60,15 @@
         finish (prompt-time "Which time the loop should end? (eg: 3:44)" (seconds->time (get current :finish 0)))]
     {:start start :finish finish}))
 
-(defn update-loop-representation [{:keys [el video]} {:keys [start finish]}]
+(defn ensure-number [n] (if (js/isNaN n) 0 n))
+
+(defn update-loop-representation [{:keys [el video]} {:keys [start finish] :as loop}]
   (let [duration (video-duration video)
         start-pct (/ start duration)
         size-pct (/ (- finish start) duration)]
     (doto el
-      (dom/set-css! "left" (str (* start-pct 100) "%"))
-      (dom/set-css! "transform" (str "scaleX(" size-pct ")")))))
+      (dom/set-css! "left" (str (ensure-number (* start-pct 100)) "%"))
+      (dom/set-css! "transform" (str "scaleX(" (ensure-number size-pct) ")")))))
 
 (defn create-loop-bar [video]
   (let [el (doto (dom/create-element! "div")
@@ -103,9 +105,10 @@
             (put! comm [:update-loop new-loop]))
         [:update-loop new-loop]
           (do
+            (.log js/console "update loop" (clj->js new-loop))
             (update-loop-representation loop-bar new-loop)
-            (video-seek! video (:start new-loop))
-            (reset! loop-ref new-loop))
+            (reset! loop-ref new-loop)
+            (if new-loop (video-seek! video (:start new-loop))))
         [:reset]
           (put! comm [:update-loop nil])
 
@@ -113,11 +116,30 @@
 
     comm))
 
+(defn item-prop [name]
+  (if-let [node (dom/$ (str "meta[itemprop=" name "]"))]
+    (.-content node)))
+
+(defn current-video-id [] (item-prop "videoId"))
+
+(defn watch-video-change []
+  (let [c (chan)
+        current-video (atom nil)]
+    (dom/observe-mutation {:container dom/body
+                           :options   {:childList false :characterData false}
+                           :callback  (fn [& _]
+                                        (let [video-id (current-video-id)]
+                                          (when (not= video-id @current-video)
+                                            (reset! current-video video-id)
+                                            (put! c video-id))))})
+    c))
+
 (defn init []
-  (dom/observe-mutation {:container dom/body
-                         :options {:childList false :characterData false}
-                         :callback (fn [& args]
-                                     (.log js/console "attributes modified on body" (clj->js args)))})
-  (init-looper (dom/$ "video")))
+  (let [looper (atom nil)]
+    (dochan [video-id (watch-video-change)]
+      (when video-id
+        (if-not @looper (reset! looper (init-looper (dom/$ "video"))))
+        (.log js/console (clj->js "video changed, reseting") video-id)
+        (>! @looper [:reset])))))
 
 (init)
