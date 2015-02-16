3a263a5c7d7c4dddff51ea01fdf7313dbd12ec9d
Wilker Lucio <wilkerlucio@gmail.com>
2015-02-16 02:47:04 -0300

    consistent styles

--- src/cljs/youtube_looper/views.cljs
+++ src/cljs/youtube_looper/views.cljs
@@ -3,35 +3,52 @@
   (:require [enfocus.core :as ef]
             [enfocus.events :as events]
             [cljs.core.async :refer [put! pipe]]
-            [youtube-looper.util :refer [seconds->time]]))
+            [youtube-looper.util :refer [seconds->time]]
+            [wilkerdev.util :refer [mapply]]))
 
 (defn loop-time [{:keys [start finish]}]
   (str (seconds->time start) " - " (seconds->time finish)))
+
+(def blue "#167ac6")
+
+(def link-style
+  {:color "#fff"
+   :cursor "pointer"
+   :text-decoration "none"})
+
+(def blue-link-style
+  (merge link-style {:color blue}))
+
+(defn set-style [styles] (mapply ef/set-style styles))
 
 (em/defsnippet loop-item :compiled "templates/yt-dialog.html"
   [".ytp-menu-row:first-child"]
   [{:keys [name] :as loop} {:keys [comm]}]
   [".ytp-menu-title"] (ef/do->
-                        (ef/set-style :cursor "pointer")
+                        (set-style link-style)
                         (ef/content name)
                         (events/listen :click #(put! comm [:rename-loop loop])))
   [".ytl-loop-time"] (ef/do->
-                       (ef/set-style :cursor "pointer")
+                       (set-style blue-link-style)
                        (ef/content (loop-time loop))
                        (events/listen :click #(put! comm [:select-loop loop])))
   [".ytl-loop-close"] (ef/do->
-                        (ef/set-style :cursor "pointer")
+                        (set-style blue-link-style)
                         (events/listen :click #(put! comm [:remove-loop loop]))))
 
 (em/defsnippet disable-loop-button :compiled "templates/yt-dialog.html"
   [".ytl-loop-disable"]
   [{:keys [comm]}]
-  ["a"] (events/listen :click #(put! comm [:select-loop nil])))
+  ["a"] (ef/do->
+          (set-style blue-link-style)
+          (events/listen :click #(put! comm [:select-loop nil]))))
 
 (em/defsnippet new-loop-button :compiled "templates/yt-dialog.html"
   [".ytl-new-loop"]
   [{:keys [comm]}]
-  ["a"] (events/listen :click #(put! comm [:pick-new-loop])))
+  ["a"] (ef/do->
+          (set-style blue-link-style)
+          (events/listen :click #(put! comm [:pick-new-loop]))))
 
 (em/deftemplate dialog-template :compiled "templates/yt-dialog.html"
   [{{:keys [loops current-loop]} :app-state :as looper}]
