8ed53d65d9d4514333e077f47fe3cb9f02a3c222
Wilker Lucio <wilkerlucio@gmail.com>
2015-04-13 21:32:13 -0300

    add parse lib and externs

---
+++ externs/parse.js
@@ -0,0 +1,16 @@
+/**
+ * @const
+ */
+var Parse = {};
+
+/**
+ * @const
+ */
+Parse.Analytics = {};
+
+/**
+ * @param {string} event
+ * @param {Object|undefined} dimensions
+ * @return {Object}
+ */
+Parse.Analytics.track = function(event, dimensions) {};
